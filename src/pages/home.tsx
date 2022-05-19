2cdb3ec76eb711872fa60d0219b61cf670e735b4
Lorenzo Lima <lorenzo.lima@etaure.srv>
2022-05-19 14:40:32 -0300

    first commit

---
+++ src/pages/home.tsx
@@ -0,0 +1,21 @@
+export function Home() {
+    return (
+        <div>
+            <aside>
+                <img src="" alt="" />
+
+                <h2>
+
+                </h2>
+                <p>
+
+                </p>
+            </aside>
+            
+            <main>
+                
+            </main>
+        </div>
+
+    )
+}
