2f0885218b82df395f50b75fdccb83b44b4f1556
Lorenzo Lima <lorenzo.lima@etaure.srv>
2022-05-19 14:58:23 -0300

    home-page creation

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,29 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+
 export function Home() {
     return (
         <div>
             <aside>
-                <img src="" alt="" />
-
-                <h2>
-
-                </h2>
-                <p>
-
-                </p>
+                <img src={illustrationImg} alt="Questions and Answers" />
+                <strong>Create live Q&amp;A rooms</strong>
+                <p>Answer the questions of your audience in real time</p>
             </aside>
             
             <main>
-                
+                <div>
+                    <img src={logoImg} alt="Letmeask" />
+                    <button>
+                        <img src={googleIconImg} alt="Google logo" />
+                        Create your room with Google
+                    </button>
+                    <div>or join</div>
+                    <form>
+                        <input type="text" placeholder='Type a room code'/>
+                        <button type='submit'>Join in the room</button>
+                    </form>
+                </div>
             </main>
         </div>
 
